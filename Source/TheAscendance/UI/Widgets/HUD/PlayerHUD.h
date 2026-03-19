a7d9e1461b6c0326ce201437a6cb0fe5e34ff6b6
Ben Higham <94679239+DidEARobya@users.noreply.github.com>
2026-03-19 14:43:40 +0000

    Fixed UI Files

--- Source/TheAscendance/UI/Widgets/HUD/PlayerHUD.h
+++ Source/TheAscendance/UI/Widgets/HUD/PlayerHUD.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "CommonUserWidget.h"
+#include "CommonActivatableWidget.h"
 #include "TheAscendance/Actors/Interaction/Interfaces/Interactable.h"
 #include "PlayerHUD.generated.h"
 
@@ -14,7 +14,7 @@ class UWidgetSwitcher;
 class UImage;
 
 UCLASS()
-class THEASCENDANCE_API UPlayerHUD : public UCommonUserWidget
+class THEASCENDANCE_API UPlayerHUD : public UCommonActivatableWidget
 {
 	GENERATED_BODY()
 	
@@ -24,21 +24,20 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void BindStatBar(UStatBoundProgressBar* statBar);
 
-	virtual void NativeConstruct() override;
-	virtual void NativeDestruct() override;
+	virtual void NativePreConstruct() override;
 
-protected:
+private:
 	void UpdateCrosshair(EInteractType targetType);
 
 private:
-	UPROPERTY(EditDefaultsOnly, meta = (BindWidget, DisplayName = "Health Bar"))
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
 	TObjectPtr<UStatBoundProgressBar> m_HealthBar = nullptr;
-	UPROPERTY(EditDefaultsOnly, meta = (BindWidget, DisplayName = "Stamina Bar"))
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
 	TObjectPtr<UStatBoundProgressBar> m_StaminaBar = nullptr;
-	UPROPERTY(EditDefaultsOnly, meta = (BindWidget, DisplayName = "Mana Bar"))
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
 	TObjectPtr<UStatBoundProgressBar> m_ManaBar = nullptr;
 
-	UPROPERTY(EditDefaultsOnly, meta = (BindWidget, DisplayName = "Action Bar"))
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
 	TObjectPtr<UActionBar> m_ActionBar = nullptr;
 
 	UPROPERTY(EditAnywhere, meta = (BindWidget))
